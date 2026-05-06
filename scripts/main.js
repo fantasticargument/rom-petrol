c47be2171abae5841fa199b3425274da5d64ca62
Petryk Roman <romic.argument@gmail.com>
2026-05-06 21:16:41 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -128,7 +128,11 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   // --- ТЕКСТОВІ ПОЛЯ ---
-  document.getElementById("modalDescription").textContent = item.description;
+  document.querySelector(".modal-title").textContent = item.title;
+  document.querySelector(".modal-category").textContent = item.category;
+  document.querySelector(".modal-availability").textContent = item.available;
+  document.querySelector(".modal-description").textContent = item.description;
+  document.querySelector(".modal-code").textContent = item.code;
 
   overlay.classList.remove("hidden");
 }
