d489f0a31e499afbf9a63519b631bf66c62e72e9
Petryk Roman <romic.argument@gmail.com>
2026-05-07 19:15:51 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -224,3 +224,12 @@ clearBtn.addEventListener("click", () => {
 
 });
 
+window.addEventListener("scroll", () => {
+  const header = document.querySelector("header");
+
+  if (window.scrollY > 50) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
